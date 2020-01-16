815ca5da89ed4f0f48cea083c285489abe07402b
Kamran Nawaz <kammy007@hotmail.com>
2020-01-16 14:13:41 +0000

    this has been an update

---
+++ build/precache-manifest.99235d81de4d5d5967728baa0b97a482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00a8311b27b2556930c6ebd13a38eee",
+    "url": "/certificatedemo/index.html"
+  },
+  {
+    "revision": "9ebfd35918dc6c82650e",
+    "url": "/certificatedemo/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "6872ea5840f3eeb021dc",
+    "url": "/certificatedemo/static/css/main.afc3a0ac.chunk.css"
+  },
+  {
+    "revision": "9ebfd35918dc6c82650e",
+    "url": "/certificatedemo/static/js/2.7843739e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/certificatedemo/static/js/2.7843739e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6872ea5840f3eeb021dc",
+    "url": "/certificatedemo/static/js/main.5219f926.chunk.js"
+  },
+  {
+    "revision": "0f0bd190f25dbdb3f412",
+    "url": "/certificatedemo/static/js/runtime-main.3fbda97c.js"
+  },
+  {
+    "revision": "ddf46f6911a51aef0362582c68ec0ced",
+    "url": "/certificatedemo/static/media/faceimage.ddf46f69.png"
+  }
+]);
